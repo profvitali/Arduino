3c06d04354e182e418117ce1f1fe42a2ec813013
profvitali <andrea.vitali@itispaleocapa.it>
2025-03-02 18:18:32 +0100

    Create operatori matematici.ino

---
+++ operatori matematici.ino
@@ -0,0 +1,12 @@
+int operando1, operando2;
+bool risultato;
+
+operando1 = 65;
+operando2 = 34;
+
+risultato = operando1 == operando2; //operando1 è uguale a operando2?
+risultato = operando1 != operando2; //operando1 è diverso a operando2?
+risultato = operando1 >= operando2; //operando1 è maggiore o uguale a operando2?
+risultato = operando1 <= operando2; //operando1 è minore o uguale a operando2?
+risultato = operando1 > operando2; //operando1 è maggiore di operando2?
+risultato = operando1 < operando2; //operando1 è minore di operando2?
