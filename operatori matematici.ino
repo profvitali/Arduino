6a41c930577824e4bc20602ac928e0feb193fcb2
profvitali <andrea.vitali@itispaleocapa.it>
2025-03-02 19:08:19 +0100

    Update and rename operatori aritmetici.ino to operatori matematici.ino

--- operatori matematici.ino
+++ operatori matematici.ino
@@ -1,4 +1,10 @@
-// in questa sezione vi spiegherò come utilizzare gli operatori aritmetici più utilizzati
+// in questa sezione vi spiegherò come utilizzare gli operatori matematici più utilizzati
+
+
+<tipo_di_dato> <nome_variabile> = <valore>
+
+
+//OPERATORI ARITMETICI
 
 //dichiarazione delle variabili variabili
 int operando1, operando2, risultato; 
@@ -49,9 +55,10 @@ void loop() {   // qui potete lasciare vuoto
 }
 
 ____________________________________________________________________________________________________________________________
+//OPERATORI RELAZIONALI
 
-int operando1, operando2;
-bool risultato;
+int operando1, operando2;  
+bool risultato;  //NB potrà essere solamente solo vero/falso
 
 operando1 = 65;
 operando2 = 34;
@@ -62,3 +69,22 @@ risultato = operando1 >= operando2; //operando1 è maggiore o uguale a operando2
 risultato = operando1 <= operando2; //operando1 è minore o uguale a operando2?
 risultato = operando1 > operando2; //operando1 è maggiore di operando2?
 risultato = operando1 < operando2; //operando1 è minore di operando2?
+
+
+//OPERATORI BOOLEANI O OPERATORI LOGICI
+
+bool op1 = true;
+bool op2 = true;
+bool op3 = false;
+
+bool risultato;
+
+risultato = op1 && op2;      //AND
+risultato = op1 && op3;
+risultato = op1 || op2;      //OR
+risultato = op1 || op3;
+risultato = !op1;            //NOT
+risultato = !op3;
+risultato = op1 && !op3;     //op1 and not_op3
+risultato = op1 || !op3;
+
