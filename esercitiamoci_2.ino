5a5b6f17acfe73f4f26927b2cad27e856a4b4d71
profvitali <andrea.vitali@itispaleocapa.it>
2026-02-27 18:01:40 +0100

    Update and rename caricami.ino to esercitiamoci_2.ino

---
+++ esercitiamoci_2.ino
@@ -0,0 +1,13 @@
+
+Utilizzando gli operatori logici si realizzi quanto di seguito:
+
+A) Realizzare il codice che scrive i numeri da 0 a 20 e li stampa uno ad uno sul monitor
+B) Leggere in input dal monitor seriale 3 numeri, calcolare la somma e indicare se la somma è maggiore di 12.
+C) Definita una variabile si calcoli la potenza di questa elevata alla 23
+D) Si deve automatizzare il sistema di riscaldamento di un laboratorio metrologico nel quale deve esser tenuta una temperatura costante per certificare le misurazioni. 
+Associare il valore ricevuto in ingresso da un sensore di temperatura (inserito da tastiera) ad una variabile. 
+Se il valore della temperatura è inferiore a 19 allora incrementare di un grado °C per volta il valore di temperatura fino ad arrivare al valore target. 
+Realizzare tale funzione con i 3 operatori logici: if, for e while. Stampare a video l'aggiornamento della temperatura. 
+Raggiunto il target aspetta 4secondi e si deve nuovamente inserire il valore.
+
+
