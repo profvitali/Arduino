e1e8f8a52809b15d42f120eceaeb06de11014e9d
profvitali <andrea.vitali@itispaleocapa.it>
2026-02-27 16:42:18 +0100

    Update variabili e costanti.ino

--- variabili e costanti.ino
+++ variabili e costanti.ino
@@ -1,10 +1,6 @@
-void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  //commento
-  // put your main code here, to run repeatedly:
-
-}
+Qualunque programma utilizzeremo farà uso di variabili per immagazzinare le informazioni da gestire. Le variabili non sono nient’altro che porzioni di memoria in cui andremo a conservare tali informazioni al fine poterle utilizzare, richiamare, modificare…
+La sintassi della variabile è la seguente:
+      <tipo_di_dato> <nome_variabile> = <valore>
+Le costanti invece rimangono invariate durante l’esecuzione del programma;
+      const <tipo_di_dato> <NOME_COSTANTE> = <valore>
+      #define <NOME_COSTANTE> <valore>
