1f05874b5f9c3dbab4cf1d93a61cf25d97da482f
profvitali <andrea.vitali@itispaleocapa.it>
2026-02-27 17:38:15 +0100

    Update esercitiamoci_1.ino

--- esercitiamoci_1.ino
+++ esercitiamoci_1.ino
@@ -1,6 +1,7 @@
 
 ESERCITIAMOCI 1 
-Eseguire lo sketch riportato di seguito ed eseguire le seguenti operazioni:
+
+  PARTE A: Eseguire lo sketch riportato di seguito ed eseguire le seguenti operazioni:
   -addizione
   -sottrazione
   -moltiplicazione 
@@ -9,8 +10,8 @@ Eseguire lo sketch riportato di seguito ed eseguire le seguenti operazioni:
   -elevare alla potenza di grado 3
   -realizzare una espressione a piacimento che usa tutti i quattro operatori contemporaneamente
 
-  definendo le variabili "base" e "altezza" si calcoli l'area di un triangolo
-  definendo "densità" "altezza" e "g" si calcoli la pressione agente sul fondo di un recipiente
+  - definendo le variabili "base" e "altezza" si calcoli l'area di un triangolo
+  - definendo "densità" "altezza" e "g" si calcoli la pressione agente sul fondo di un recipiente
   
 
 ________________________________________________________________________________________________  
@@ -28,3 +29,14 @@ void setup() {
 
 void loop() {   // qui potete lasciare vuoto  
 }
+
+
+________________________________________________________________________________________________ 
+PARTE B: utilizzando quanto fatto sopra, programmare i sequenti confronti:
+  -operando1 è uguale a operando2?
+  -operando1 è diverso da operando2?
+  -operando1 è maggiore o uguale a operando2?
+  -operando1 è minore a operando2?
+
+  -  definendo "densità" "altezza" e "g" e "pressione_critica" si calcoli se la pressione agente sul fondo di un recipiente risulta inferiore a quella critica
+
